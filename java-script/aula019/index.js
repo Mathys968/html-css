348686c2512a84e515f6f7ccdafc9afc7d12d535
MateusSoa <73123163+Mathys968@users.noreply.github.com>
2023-12-06 19:29:13 -0300

    while, do while, for/ Enquanto, faça Enquanto, Para

---
+++ java-script/aula019/index.js
@@ -0,0 +1,39 @@
+/*var agora = new Date()
+var hora = agora.getMinutes
+var n1 = 1
+do{
+    n1++ <- recebe +1
+    console.log(n1)
+}while(hora = 16)*/
+
+
+/*while(){
+    
+}*/
+ 
+/*for(var c = 1;c <= 10; c++){
+ console.log(c)   
+}*/
+
+var valor = 3
+var n1 = 0
+var n2 = 10
+if(valor == 1){
+    valor = n1
+    while(valor < 10){
+    valor++
+    console.log(valor)
+}
+}
+else if( valor == 2){
+    valor = n2
+while(valor > 0){
+    valor = valor - 1
+    console.log(valor)
+}
+}
+else{
+    console.log('Adeus...')
+}
+
+
---
+++ java-script/aula019/node_97fa504be80ad.tmp.js
@@ -0,0 +1,8 @@
+var pizza = 8
+while(pizza > 0){
+ pizza = pizza - 1
+ console.log(pizza)
+ if(pizza = 4){
+    pizza += 1
+ }
+} 
