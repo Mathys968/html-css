1e0bddc61bec5f89a68d027d38fd875af6449028
MateusSoa <73123163+Mathys968@users.noreply.github.com>
2023-12-07 16:49:56 -0300

    Array

---
+++ java-script/aula022/index.js
@@ -0,0 +1,28 @@
+/*let valor = [5, 4, 7]
+valor[3] = 11
+console.log(`Nosso vetor é o ${valor}`)
+for(valor; valor; valor[3] = valor[3] - 1){
+    valor.push('Oin')   
+}
+ console.log(valor, valor.length)*/
+/*
+ let valor = [12, 1243, 141, 124]
+ for(let pos in valor){
+    console.log(valor[pos])
+ } ex
+ */
+/*
+ let v = [1, 2, 4, 5, 6]
+ console.log(v.indexOf(3))*/
+/* FIBONACCI
+ var C = 3
+ var t1 = 0
+ var t2 = 1
+ var t3 = 0
+ for(var C; C<=15;t3 = t1 + t2){
+    t1 = t2
+    t2 = t3
+    C++
+    console.log(t3)
+ }
+ */
