7fb98a9e9a429334315f8b87f778f6fce9e03d6b
MateusSoa <73123163+Mathys968@users.noreply.github.com>
2023-12-21 17:02:33 -0300

    Terminado

--- java-script/ex034/index.js
+++ java-script/ex034/index.js
@@ -1,17 +1,20 @@
 let Hom = 0
+let AltH = []
 let Mul = 0
    function Enviar(){
     let sexo = document.getElementsByName('sex')
     let altu = document.getElementById('alt')
-    let alt = []
     let gênero = ''
     let res = document.getElementById('res')
     
     //gênero//
     if(sexo[0].checked){
         gênero = 'Homem'
+        Hom++
+        AltH.push(Number(altu.value))
     } else if(sexo[1].checked){
         gênero = 'Mulher'
+        Mul++
     } else{
         'Marque algo'
     }
@@ -20,11 +23,45 @@ let Mul = 0
         window.alert('TITÃ COLOSSAL????')
     }
 
- 
-
-    //CALC//
     
- alt.push(Number(altu.value))
+
+    
+ 
+    //CALC//
+    if(gênero == 'Homem'){
+        res.innerHTML = ''
+        res.innerHTML += `REGISTRO FEITO!!` 
+    } else if(gênero == 'Mulher'){
+        res.innerHTML = ''
+        res.innerHTML += `REGISTRO FEITO!!`
+    }else{
+        res.innerHTML += 'ERRO'
+    }
+
+}
+
+
+
+function Confi(){ 
+    res.innerHTML = ''
+    let soma = 0
+    let maior = AltH[0]
+    let menor = AltH[0]
+    for(let i = 0; i < AltH.length; i++){
+    soma += AltH[i]
+    if(AltH[i] > maior){
+        maior = AltH[i]
+    } else if(AltH[i] < menor){
+        menor = AltH[i]
+    }
+    }
+    res.innerHTML += `<br> Essa é a média de altura entre os homens, Média: ${soma/Hom}`
+    res.innerHTML += `<br> Essa é a quantidade de mulheres registrada: ${Mul}`
+    res.innerHTML += `<br> A maior altura entre eles é: ${maior}`
+    res.innerHTML += `<br> A menor altura entre eles é ${menor}`
+}
+/*
+alt.push(Number(altu.value))
     let maior = alt[0]
     let menor = alt[0]
     for(let pos in alt){
@@ -33,24 +70,6 @@ let Mul = 0
         }
     }
 
-    if(gênero == 'Homem'){
-        res.innerHTML = ''
-        Hom++
-        res.innerHTML += `Um Homem com a altura ${Number(altu.value)} ${Hom} ${maior}.`
-        
-    } else if(gênero == 'Mulher'){
-        res.innerHTML += `Uma Mulher com a a altura ${Number(altu.value)} ${Mul}.`
-    }else{
-        res.innerHTML += 'ERRO'
-    }
-
-   
-
-    res.innerHTML += `O maior de todos tem a altura ${maior}`
-}
-
-
-/*
  let i = 1 + 1
  for(let i = 0; i < 10; i++){
     Enviar()
