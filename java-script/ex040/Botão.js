5513bc540273654827bf7cb88adf14d951617842
MateusSoa <73123163+Mathys968@users.noreply.github.com>
2023-12-28 17:17:39 -0300

    test

---
+++ java-script/ex040/Botão.js
@@ -0,0 +1,7 @@
+function Click(){
+    let body = document.getElementById('cabe')
+    let parta = document.getElementById('bru')
+    body.style.backgroundColor = 'White'
+    body.innerHTML = '<h1> Está funcionando </h1>'
+    
+}
