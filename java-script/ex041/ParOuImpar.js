14d9e54e2e2284eec5c622585c6a55a7f056b29b
MateusSoa <73123163+Mathys968@users.noreply.github.com>
2023-12-29 13:54:38 -0300

    .

---
+++ java-script/ex041/ParOuImpar.js
@@ -0,0 +1,10 @@
+function ParOuImpar(n){
+let Par = n/2
+if(n % 2 == 0){
+return true
+} else{
+    return false
+}
+
+}
+console.log(ParOuImpar(300))
---
+++ java-script/ex042/index.js
@@ -0,0 +1,9 @@
+let num = [2, 3, 12, 12, 32]
+function Media(vum){
+    let s = 0
+for(vum; vum <= num.length; vum++){
+    s += num[vum]
+    return s
+}
+}
+console.log(Media())
