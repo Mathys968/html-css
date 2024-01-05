1588a5d51b906246201723033af75641684e81fb
MateusSoa <73123163+Mathys968@users.noreply.github.com>
2024-01-05 18:58:37 -0300

    Mais Map, Filter, Reduce

--- java-script/ex045/aula005.js
+++ java-script/ex045/aula005.js
@@ -1,4 +1,49 @@
-const cursos = ['HTML', 'CSS', 'JS', 'PHP', 'REACT']
-cursos.map((el,ind) => {
-    console.log(`Curso ${el} - posição do curso ${ind}`)
+/*
+MAAP
+const cursos = 
+[
+{nome: 'CSS', custo: 43},
+{nome: 'JS', custo: 221},
+{nome: 'HTML', custo: 21}
+];
+const nomes = cursos.map(elementos => elementos.nome);
+const custo = cursos.map(elementos => elementos.custo);
+console.log(custo)
+
+console.log(cursos)
+
+const duplicata = cursos.map((x, i) => {
+console.log(x)
+
 })
+*/
+
+
+
+
+/* FILTER
+const cursos = 
+[
+{nome: 'CSS', custo: 43},
+{nome: 'JS', custo: 221},
+{nome: 'HTML', custo: 21}
+];
+
+const cust = cursos.filter(nome => nome.nome === 'CSS').map(name => name.nome)
+console.log(cust)
+*/
+
+
+
+
+
+/* REDUCE
+const cursos = 
+[
+{nome: 'CSS', custo: 43},
+{nome: 'JS', custo: 221},
+{nome: 'HTML', custo: 21}
+];
+
+const total = cursos.reduce((acc, p) => acc+ p.custo, 0)
+console.log(total)*/
